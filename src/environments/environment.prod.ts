530157aaef1c92bb9555cc40ef29116c10feca46
Niklas <nkreutzarek@hotmail.de>
2018-09-23 16:53:18 +0200

    Add environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiEndpoints: {
-    CONVERSATION_API_URL: '',
+    CONVERSATION_API_URL: 'https://service.eu-de.apiconnect.ibmcloud.com/gws/apigateway/api/05228ef049045b87490b99e65d97270739d670d9ebb2ea5d5684c205ebd7deb6/iwibot/router',
     HSKA_STUDENT_INFO_URL: 'https://www.iwi.hs-karlsruhe.de/Intranetaccess/REST/credential/v2/info',
     BULLETIN_BOARD_NEWS_URL: 'https://www.iwi.hs-karlsruhe.de/Intranetaccess/REST/newsbulletinboard',
   }
