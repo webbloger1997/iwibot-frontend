d050d2bf1999b80ad785919d51245af5fc219d3b
Niklas <nkreutzarek@hotmail.de>
2018-11-15 23:58:21 +0100

    Fix CONVERSATION_API_URL for production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiEndpoints: {
-    CONVERSATION_API_URL: 'https://service.eu-de.apiconnect.ibmcloud.com/gws/apigateway/api/05228ef049045b87490b99e65d97270739d670d9ebb2ea5d5684c205ebd7deb6/iwibot/router',
+    CONVERSATION_API_URL: 'https://service.us.apiconnect.ibmcloud.com/gws/apigateway/api/c9f88de3acb5a4648e4f118769d019c8df8797d1777c4342f43260626b4c51bf/iwibot/router',
     HSKA_STUDENT_INFO_URL: 'https://www.iwi.hs-karlsruhe.de/Intranetaccess/REST/credential/v2/info',
     BULLETIN_BOARD_NEWS_URL: 'https://www.iwi.hs-karlsruhe.de/Intranetaccess/REST/newsbulletinboard',
   }
