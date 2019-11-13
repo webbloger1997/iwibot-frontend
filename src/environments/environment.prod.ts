6d35f2a3bf6d9cc900a625c56edfa8d1fbc71e33
webbloger1997 <kevin.galm@icloud.com>
2019-11-13 13:09:22 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiEndpoints: {
-    CONVERSATION_API_URL: 'https://a5c3441f.us-south.apiconnect.appdomain.cloud/iwibotapi/router',
+    CONVERSATION_API_URL: 'https://2e76165a.eu-gb.apiconnect.appdomain.cloud/iwibot/router',
     HSKA_STUDENT_INFO_URL: 'https://www.iwi.hs-karlsruhe.de/iwii/REST/credential/v2/info',
     BULLETIN_BOARD_NEWS_URL: 'https://www.iwi.hs-karlsruhe.de/iwii/REST/newsbulletinboard',
     HSKA_LIB_BORROWED_BOOKS_URL: 'https://www.iwi.hs-karlsruhe.de/iwii/REST/library/borrowedbooks',
